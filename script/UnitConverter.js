4d4495714bcb3846618290df827d94fe9027122e
Anupama Sajja <anupamasajja18@gmail.com>
2022-12-05 22:49:57 -0600

    Unit Converter project changes

---
+++ script/UnitConverter.js
@@ -0,0 +1,17 @@
+let convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+
+convertBtn.addEventListener("click", function() {
+    let baseValue = inputEl.value
+    
+    lengthEl.textContent = `${baseValue} meters = ${baseValue * meterToFeet} feet | ${baseValue} feet = ${baseValue / meterToFeet} meters`
+    volumeEl.textContent = `${baseValue} liters = ${baseValue * literToGallon} gallons | ${baseValue} gallons = ${baseValue / literToGallon} liters`
+    massEl.textContent = `${baseValue} kilos = ${baseValue * kiloToPound} pounds | ${baseValue} pounds = ${baseValue / kiloToPound} kilos`
+})
